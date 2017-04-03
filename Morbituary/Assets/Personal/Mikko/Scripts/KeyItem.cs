4fbfd3c28aa98aba1ac602ccadbd64e63dab26cf
Mikko Paappanen <mikko_paappanen@hotmail.com>
2017-04-03 11:00:17 +0300

    Pick up keys, open gate with a key

---
+++ Morbituary/Assets/Personal/Mikko/Scripts/KeyItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItem : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Player")
+        {
+            GameVariables.key += 1;
+            Debug.Log(GameVariables.key);
+            Destroy(gameObject);
+        }
+    }
+}
