4ead305486f3130c1163014fed6b1ed65668019d
jussivesa <jussi.vesa@gmail.com>
2017-04-04 11:12:41 +0300

    Add basic dmg to Enemy when colliders hit, basic death also.

--- Morbituary/Assets/Code/Actors/Enemy.cs
+++ Morbituary/Assets/Code/Actors/Enemy.cs
@@ -11,21 +11,93 @@ namespace Assets.Code.Actors
 	class Enemy : Actor<IAttack>
 	// Use this for initialization
 	{
-		void Start()
+        public int Damage { get; set; }
+        public float timeBetweenAttacks = 0.5f;
+        float timer;
+        bool playerInRange;
+        
+        GameObject PlayerGameObject;
+        GameObject EnemyGameOnject;
+        Player target;
+        Player Player;
+        Enemy Enem;
+        GameObject[] enemiesArray;
+
+        void Awake()
+        {
+
+            // Get enemies to array
+            enemiesArray = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemiesArray)
+            {
+                Enem = ToEnemy(enemy);
+                Enem.Damage = 3;
+            }
+
+            Player Player = Player.ToPlayer(Player.GetPlayer());
+            PlayerGameObject = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        void Start()
 		{
 
-		}
+            
+        }
 
 		// Update is called once per frame
 		void Update()
 		{
 
-		}
+            // Add the time since Update was last called to the timer.
+            timer += Time.deltaTime;
+
+            if (timer >= timeBetweenAttacks && playerInRange)
+            {
+                Attack(target);
+            }
+
+
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            // If the entering collider is the player...
+            if (other.gameObject == PlayerGameObject)
+            {
+                // ... the player is in range.
+                playerInRange = true;
+                target = Player.ToPlayer(PlayerGameObject);
+            }
+        }
+
+
+        void OnTriggerExit(Collider other)
+        {
+            // If the exiting collider is the player...
+            if (other.gameObject == PlayerGameObject)
+            {
+                // ... the player is no longer in range.
+                playerInRange = false;
+                target = null;
+            }
+        }
+
+        protected void Attack(Player target)
+        {
+            Debug.Log("Enemy Attack() here, target: " + target);
+            // Reset the timer.
+            timer = 0f;
+            // Deal damage
+            target.receiveDamage(Damage);
+        }
 
         protected override void OnDeath()
         {
             Status = ActorStatus.Dead;
             Debug.Log(this + " is now dead");
+            // TODO: this should be replaced by death animation
+            Destroy(this.gameObject);
+
         }
 
 		public static Enemy ToEnemy(GameObject gameObject)
