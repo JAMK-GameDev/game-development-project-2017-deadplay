90c28bc9f27660d70529dd797543b5753a8855aa
jussivesa <jussi.vesa@gmail.com>
2017-04-04 11:34:10 +0300

    Add frequency for attacking.

--- Morbituary/Assets/Code/playerController.cs
+++ Morbituary/Assets/Code/playerController.cs
@@ -8,25 +8,27 @@ using Assets.Code.Actors;
 
 public class playerController : MonoBehaviour
 {
-
-	KeyCode sb = KeyCode.Space;
-	Weapon wp = new Weapon();
+    Weapon wp = new Weapon();
     GameObject sword1;
     BoxCollider bx;
     GameObject[] enemiesArray;
 	bool enemyInRange;
-	Enemy target;
+    bool canHit;
+    private float lastAttack;
+    Enemy target;
 
 	void Awake()
 	{
-		// TODO: How to initialize new weapon? Active weapon if more than 1?
-		wp.Name = "Shitty sword";
+        // TODO: How to initialize new weapon? Active weapon if more than 1?
+        wp.Name = "Shitty sword";
 		wp.Damage = 10;
 		wp.Range = 3.1f;
+        wp.Frequency = 1.3f;
 
-        // Change weapon collider size based on weapon range, done for Players child object "Sword1"
-        // TODO: needs better implementation
-        sword1 = GameObject.Find("Sword1");
+
+    // Change weapon collider size based on weapon range, done for Players child object "Sword1"
+    // TODO: needs better implementation
+    sword1 = GameObject.Find("Sword1");
         BoxCollider boxCollider = sword1.GetComponent<BoxCollider>();
         if (boxCollider != null)
         {
@@ -35,7 +37,6 @@ public class playerController : MonoBehaviour
         }
         // Get enemies to array
         enemiesArray = GameObject.FindGameObjectsWithTag("Enemy");
-
 	}
 
 	// Use this for initialization
@@ -75,17 +76,27 @@ public class playerController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		// If pressed Spacebar (Attack input key)
-		if (Input.GetKeyDown(sb))
+        
+
+
+        // Attack input with freq, not allowing while blocking
+        if (Time.time > wp.Frequency + lastAttack && Input.GetMouseButtonDown(0) && canHit && wp.IsInRange(target))
 		{
-            if (wp.IsInRange(target))
-            {
-                Debug.Log("In range, calling DealDamage()");
-                //target.receiveDamage(wp);
-                wp.DealDamage(target);
-            }
-            
+            Debug.Log("In range, calling DealDamage()");
+            //target.receiveDamage(wp);
+            wp.DealDamage(target);
+
+            lastAttack = Time.time;
 		}
+        // Block input key
+        if (Input.GetMouseButton(1))
+        {
+            canHit = false;
+            Debug.Log("Blocking");
+        } else {
+            //released 
+            canHit = true;
+        }
 
 	}
 }
