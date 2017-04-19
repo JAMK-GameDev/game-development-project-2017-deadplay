ab8c90f389355fdfb4fd26bfd922296b4d10ba01
jussivesa <jussi.vesa@gmail.com>
2017-04-19 14:19:01 +0300

    Add weapon direction change on player movement. Start of dmg text popups.

---
+++ Morbituary/Assets/Code/UIPopupText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPopupText : MonoBehaviour {
+
+    public Animator animator;
+    private Text dmgText;
+
+    void Start()
+    {
+        AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+        // Destroy popup text when anim ends
+        Destroy(gameObject, clipInfo[0].clip.length);
+        dmgText = animator.GetComponent<Text>();
+
+    }
+
+    public void SetText(string text)
+    {
+        dmgText.text = text;
+    }
+}
---
+++ Morbituary/Assets/Code/UIPopupTextController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPopupTextController : MonoBehaviour {
+
+    private static UIPopupText popupText;
+
+    public static void Initialize()
+    {
+        popupText = Resources.Load<UIPopupText>("/Prefabs/Popup Text Parent");
+    }
+
+    public static void CreatePopupText(string text, Transform location)
+    {
+
+    }
+
+
+}
